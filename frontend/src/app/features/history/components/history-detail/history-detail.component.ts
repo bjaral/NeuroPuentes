28d4df5538ffb750fc988983371a2fb46f61b935
bjaral <benjaminjara1505@gmail.com>
2025-08-16 19:18:35 -0400

    chore(frontend): añadir estructura base

---
+++ frontend/src/app/features/history/components/history-detail/history-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-history-detail',
+  imports: [],
+  templateUrl: './history-detail.component.html',
+  styleUrl: './history-detail.component.scss'
+})
+export class HistoryDetailComponent {
+
+}
---
+++ frontend/src/app/features/scenarios/components/scenarios/scenarios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scenarios',
+  imports: [],
+  templateUrl: './scenarios.component.html',
+  styleUrl: './scenarios.component.scss'
+})
+export class ScenariosComponent {
+
+}
