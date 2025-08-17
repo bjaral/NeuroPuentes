ab517f5c06342eb2f03cb332f8202851956250d7
Paula <psantanav0@gmail.com>
2025-08-17 17:31:37 -0400

    Vista Feedback lista (HTML + SCSS), incluyendo ruta

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FeedbackComponent } from './features/feedback/components/feedback/feedback.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { 'path': 'feedback', 'component': FeedbackComponent }
+    
+];
