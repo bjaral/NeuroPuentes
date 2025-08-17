ace60b79e1acdbb46c81775c31d05e5afc00defa
bjaral <benjaminjara1505@gmail.com>
2025-08-17 18:18:53 -0400

    feat(frontend): Simulación a nivel visual con entrevista falsa

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/components/login/login.component';
+import { SimulationComponent } from './features/simulation/components/simulation/simulation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 'path': 'login', 'component': LoginComponent },
+    { 'path': 'simulation', 'component': SimulationComponent },
+
+];
