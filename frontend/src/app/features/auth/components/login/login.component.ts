742aeac6cbe2643578297f12caa3b6233356575b
bjaral <benjaminjara1505@gmail.com>
2025-08-20 21:49:48 -0400

    feat(frontend): login avanzado

--- frontend/src/app/features/auth/components/login/login.component.ts
+++ frontend/src/app/features/auth/components/login/login.component.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../../core/services/auth.service';
+import { MATERIAL_IMPORTS } from '../../../../shared/material/material';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ...MATERIAL_IMPORTS, RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  hidePassword = true;
+  loginForm: any;
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const credentials = {
+        correo: this.loginForm.value.email,
+        password: this.loginForm.value.password
+      };
+
+      this.authService.login(credentials).subscribe({
+        next: (res) => {
+          if (res && res.token) {
+            localStorage.setItem('tokenNeuroPuentes', res.token);
+          } else {
+            // Manejar caso donde no hay token en la respuesta
+          }
+          console.log('Inicio de sesión exitoso');
+          this.toPerfil();
+        },
+        error: (err) => {
+          alert('Error en el inicio de sesión');
+        }
+      });
+    } else {
+      console.error('Formulario no válido');
+    }
+  }
+
+  toPerfil() {
+    this.router.navigate(['/']);
+  }
 
 }
